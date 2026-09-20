760e66481dcfdd80f5d6aa756ccb719d8792f70f
Abolfazl yousof nezhad <y3606671@gmail.com>
2026-09-20 12:57:57 +0330

    Update main.py

--- main.py
+++ main.py
@@ -56,4 +56,4 @@ def creatmodel():
 
     return w, b
 
-model(np.array([100])) # Enter the desired Fahrenheit temperature here
+model(np.array([int(input("Farenheit :"))])) # Enter the desired Fahrenheit temperature here
